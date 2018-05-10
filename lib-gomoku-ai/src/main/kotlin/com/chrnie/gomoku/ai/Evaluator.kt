ec1c71341eea28c3d7e7b355c774fe4042f12229
chrnie <chrnie@qq.com>
2018-05-10 23:34:16 +0800

    Fix statistics score table

--- lib-gomoku-ai/src/main/kotlin/com/chrnie/gomoku/ai/Evaluator.kt
+++ lib-gomoku-ai/src/main/kotlin/com/chrnie/gomoku/ai/Evaluator.kt
@@ -105,7 +105,7 @@ object EvaluatorImpl : Evaluator {
                     2 to mapOf(false to 100, true to 10),
                     3 to mapOf(false to 1000, true to 100),
                     4 to mapOf(false to 10000, true to 1000),
-                    5 to mapOf(false to 100000, true to 10000)
+                    5 to mapOf(false to 100000, true to 100000)
             )
         }
 
