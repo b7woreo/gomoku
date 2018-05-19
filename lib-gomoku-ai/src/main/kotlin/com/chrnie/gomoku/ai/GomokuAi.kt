7b4d3f24864f5e7ab132e148d0d79024d4d66fbf
chrnie <chrnie@qq.com>
2018-05-19 20:44:59 +0800

    Implement alphabeta

--- lib-gomoku-ai/src/main/kotlin/com/chrnie/gomoku/ai/GomokuAi.kt
+++ lib-gomoku-ai/src/main/kotlin/com/chrnie/gomoku/ai/GomokuAi.kt
@@ -2,93 +2,12 @@ package com.chrnie.gomoku.ai
 
 import com.chrnie.gomoku.Chessman
 import com.chrnie.gomoku.GomokuGame
-import java.util.*
-import kotlin.collections.HashSet
-import kotlin.math.max
-import kotlin.math.min
 
-class GomokuAi(private val player: Chessman, private val game: GomokuGame) {
+abstract class GomokuAi(protected val player: Chessman, protected val game: GomokuGame) {
 
-    companion object {
-        private const val MINIMAX_INIT_DEPTH = 2
-    }
+    abstract fun next(): Point
 
-    private val random = Random(System.currentTimeMillis())
-    private val evaluator = EvaluatorImpl
-
-    fun next(): Point {
-        if (game.chessman != player) {
-            throw RuntimeException("current not $player play")
-        }
-
-        if (game.isWin) {
-            throw RuntimeException("game is win")
-        }
-
-        var maxScore = Int.MIN_VALUE
-        val resultList = ArrayList<Point>()
-
-        val pointList = gen()
-        for (p in pointList) {
-            val score = minimax(p.x, p.y, MINIMAX_INIT_DEPTH)
-            if (score < maxScore) {
-                continue
-            }
-            if (score > maxScore) {
-                maxScore = score
-                resultList.clear()
-            }
-            resultList.add(p)
-        }
-
-        if (resultList.isEmpty()) {
-            var p: Point? = null
-            while (p == null) {
-                val x = random.nextInt(game.chessboardWidth)
-                val y = random.nextInt(game.chessboardHeight)
-                if (game.chessmanAt(x, y) != null) {
-                    continue
-                }
-                p = Point(x, y)
-            }
-
-            return p
-        }
-
-        val r = random.nextInt(resultList.size)
-        return resultList[r]
-    }
-
-    private fun minimax(x: Int, y: Int, depth: Int): Int {
-        game.putChessman(x, y)
-
-        if (game.isWin || depth == 0) {
-            game.undo()
-            return evaluator.evaluate(game, player)
-        }
-
-        val pointList = gen()
-
-        var score: Int
-        if (player == game.chessman) {
-            score = Int.MIN_VALUE
-
-            for (p in pointList) {
-                score = max(score, minimax(p.x, p.y, depth - 1))
-            }
-        } else {
-            score = Int.MAX_VALUE
-
-            for (p in pointList) {
-                score = min(score, minimax(p.x, p.y, depth - 1))
-            }
-        }
-
-        game.undo()
-        return score
-    }
-
-    private fun gen(): List<Point> {
+    protected fun findPutChessmanPoint(): List<Point> {
         val set = HashSet<Point>()
 
         val width = game.chessboardWidth
@@ -113,9 +32,4 @@ class GomokuAi(private val player: Chessman, private val game: GomokuGame) {
 
         return set.filter { (x, y) -> (x in 0 until width) && (y in 0 until height) && (game.chessmanAt(x, y) == null) }
     }
-
-    data class Point(
-            val x: Int,
-            val y: Int
-    )
 }
---
+++ lib-gomoku-ai/src/main/kotlin/com/chrnie/gomoku/ai/Point.kt
@@ -0,0 +1,6 @@
+package com.chrnie.gomoku.ai
+
+data class Point(
+        val x: Int,
+        val y: Int
+)
