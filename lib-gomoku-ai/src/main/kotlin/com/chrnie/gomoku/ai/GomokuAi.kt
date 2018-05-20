4d38cc7547b8c232a15255a9ab9f7a1c8046cf48
chrnie <chrnie@qq.com>
2018-05-20 15:21:53 +0800

    AI 不再固定只下一种颜色棋子

--- lib-gomoku-ai/src/main/kotlin/com/chrnie/gomoku/ai/GomokuAi.kt
+++ lib-gomoku-ai/src/main/kotlin/com/chrnie/gomoku/ai/GomokuAi.kt
@@ -2,12 +2,103 @@ package com.chrnie.gomoku.ai
 
 import com.chrnie.gomoku.Chessman
 import com.chrnie.gomoku.GomokuGame
+import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 
-abstract class GomokuAi(protected val player: Chessman, protected val game: GomokuGame) {
+class GomokuAi(val game: GomokuGame, val difficulty: Int) {
 
-    abstract fun next(): Point
+    private val random = Random(System.currentTimeMillis())
+    private val evaluator = EvaluatorImpl
 
-    protected fun findPutChessmanPoint(): List<Point> {
+    init {
+        if (difficulty <= 0 || difficulty % 2 != 0) {
+            throw RuntimeException("difficulty must > 0")
+        }
+    }
+
+    fun next(): Point {
+        if (game.isWin) {
+            throw RuntimeException("game is win")
+        }
+
+        var maxScore = Int.MIN_VALUE
+        val resultList = ArrayList<Point>()
+
+        val pointList = findPutChessmanPoint()
+        for (p in pointList) {
+            val score = alphabeta(p.x, p.y, difficulty, Int.MIN_VALUE, Int.MAX_VALUE, game.chessman)
+            if (score < maxScore) {
+                continue
+            }
+            if (score > maxScore) {
+                maxScore = score
+                resultList.clear()
+            }
+            resultList.add(p)
+        }
+
+        if (resultList.isEmpty()) {
+            var p: Point? = null
+            while (p == null) {
+                val x = random.nextInt(game.chessboardWidth)
+                val y = random.nextInt(game.chessboardHeight)
+                if (game.chessmanAt(x, y) != null) {
+                    continue
+                }
+                p = Point(x, y)
+            }
+
+            return p
+        }
+
+        val r = random.nextInt(resultList.size)
+        return resultList[r]
+    }
+
+    private fun alphabeta(x: Int, y: Int, depth: Int, alpha: Int, beta: Int, player: Chessman): Int {
+        println("($x,$y) - depth:$depth - alpha:$alpha - beta:$beta")
+
+        game.putChessman(x, y)
+
+        if (game.isWin || depth == 0) {
+            game.undo()
+            return evaluator.evaluate(game, player)
+        }
+
+        val pointList = findPutChessmanPoint()
+
+        var a = alpha
+        var b = beta
+
+        var score: Int
+        if (player == game.chessman) {
+            score = Int.MIN_VALUE
+
+            for (p in pointList) {
+                score = max(score, alphabeta(p.x, p.y, depth - 1, a, b, player))
+                a = max(a, score)
+                if (b <= a) {
+                    break
+                }
+            }
+        } else {
+            score = Int.MAX_VALUE
+
+            for (p in pointList) {
+                score = min(score, alphabeta(p.x, p.y, depth - 1, a, b, player))
+                b = min(score, b)
+                if (b <= a) {
+                    break
+                }
+            }
+        }
+
+        game.undo()
+        return score
+    }
+
+    private fun findPutChessmanPoint(): List<Point> {
         val set = HashSet<Point>()
 
         val width = game.chessboardWidth
