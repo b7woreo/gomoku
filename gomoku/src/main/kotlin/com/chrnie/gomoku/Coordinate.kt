adcdbddc7c1c8d6ffabe15be8f7e3bc8de03211e
chrnie <chrnie@foxmail.com>
2021-04-09 00:23:14 +0800

    refactor：Coordinate 改为 inline class 实现，提升性能

--- gomoku/src/main/kotlin/com/chrnie/gomoku/Coordinate.kt
+++ gomoku/src/main/kotlin/com/chrnie/gomoku/Coordinate.kt
@@ -1,9 +1,17 @@
 package com.chrnie.gomoku
 
-data class Coordinate(val x: Int, val y: Int) {
-    fun move(direction: Direction, delta: Int): Coordinate {
-        return Coordinate(x + (direction.dx * delta), y + (direction.dy * delta))
-    }
+fun Coordinate(x: Int, y: Int): Coordinate =
+    CoordinateImpl((x.toLong() shl Int.SIZE_BITS) or (y.toLong() and 0xFFFFFFFF))
+
+interface Coordinate {
+    val x: Int
+    val y: Int
+
+    fun move(direction: Direction, delta: Int): Coordinate
+
+    operator fun component1() = x
+
+    operator fun component2() = y
 }
 
 enum class Direction(val dx: Int, val dy: Int) {
@@ -43,3 +51,20 @@ enum class Direction(val dx: Int, val dy: Int) {
     abstract fun reverse(): Direction
 
 }
+
+private inline class CoordinateImpl(private val value: Long) : Coordinate {
+
+    override val x: Int
+        get() = (value shr Int.SIZE_BITS).toInt()
+
+    override val y: Int
+        get() = value.toInt()
+
+    override fun move(direction: Direction, delta: Int): Coordinate {
+        return Coordinate(x + (direction.dx * delta), y + (direction.dy * delta))
+    }
+
+    override fun toString(): String = "($x, $y)"
+}
+
+
--- gomoku/src/test/kotlin/com/chrnie/gomoku/CoordinateTest.kt
+++ gomoku/src/test/kotlin/com/chrnie/gomoku/CoordinateTest.kt
@@ -4,6 +4,36 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class CoordinateTest {
+    
+    @Test
+    fun `test x,y`() {
+        Coordinate(0,0).run {
+            assertEquals(0, x)
+            assertEquals(0, y)
+        }
+
+        Coordinate(Int.MAX_VALUE, Int.MIN_VALUE).run {
+            assertEquals(Int.MAX_VALUE, x)
+            assertEquals(Int.MIN_VALUE, y)
+        }
+
+        Coordinate(Int.MIN_VALUE, Int.MAX_VALUE).run {
+            assertEquals(Int.MIN_VALUE, x)
+            assertEquals(Int.MAX_VALUE, y)
+        }
+    }
+    
+    @Test
+    fun `test destructuring`() {
+        val (x, y) = Coordinate(1, -1)
+        assertEquals(1, x)
+        assertEquals(-1, y)
+    }
+    
+    @Test
+    fun `test equals`() {
+        assertEquals(Coordinate(0, 0), Coordinate(0, 0))
+    }
 
     @Test
     fun testCoordinate_move() {
